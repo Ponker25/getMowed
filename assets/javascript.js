92ee772a9cd3bdc618fedee64a9d96ef628d94ce
Ponker25 <williamswensen86@gmail.com>
2018-06-07 20:53:31 -0600

    beginnings of ajax call

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,19 +1,22 @@
 console.log ("starting")
 
-var apiKey = "&api_key=C1609E9E-F9B9-41A6-B33B-DBBF74943CBB";
+var aqiLat = $("#cityInputLat").text();
+var aqiLong = $("#cityInputLong").text();
 
-    var airQuality = $(this).attr("data-air");
-    var imageType = $(this).text();
-    var queryURL = "https//www.airnowapi.org/aq/forecast/zipCode/?format=text/csv&zipCode=84106&date=2018-06-07&distance=25&API_KEY=C1609E9E-F9B9-41A6-B33B-DBBF74943CBB";
- 
-  
-  // function for pulling air quality when button is clicked
-  $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      .then(function(response) {
-        var results = response.data;
-        console.log(response);
+// var date = moment.js() ;
+var apiKey = "&API_KEY=C1609E9E-F9B9-41A6-B33B-DBBF74943CBB";
 
-    })
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://api.breezometer.com/baqi/?lat=40.7324296&lon=-73.9977264&key=e4417ac440f444eb8397e28bcb3fc5c5",
+  "method": "GET",
+  "headers": {
+    "Cache-Control": "no-cache",
+    "Postman-Token": "1d63ebfc-0c70-4f76-ad2e-88c75e87c36c"
+  }
+}
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
